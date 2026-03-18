560b36d0af47c7a0e444eea596fd60bdbee4b60b
Dawid-Piotrowski-s33551 <s33551@pjwstk.edu.pl>
2026-03-18 01:36:19 +0100

    rest of methods implemented on Main

--- MojProjekt/Main.cs
+++ MojProjekt/Main.cs
@@ -24,9 +24,32 @@ namespace MojProjekt
                 Console.WriteLine("Error: These are not integers.");
                 return;
             }
-            
+
             int total = StatisticsHelper.Sum(num1, num2);
             Console.WriteLine($"Sum: {total}");
+
+            Console.WriteLine("Enter third number: ");
+            var in3 = Console.ReadLine();
+            int num3;
+            try
+            {
+                num3 = int.Parse(in3);
+            }
+            catch
+            {
+                Console.WriteLine("Error: These are not integers.");
+                return;
+            }
+
+            int[] values = new int[]{num1, num2, num3};
+
+            double average = StatisticsHelper.CalculateAverage(values);
+            int min = StatisticsHelper.CalculateMin(values);
+            int max = StatisticsHelper.CalculateMax(values);
+
+            Console.WriteLine($"Average: {average}");
+            Console.WriteLine($"Min: {min}");
+            Console.WriteLine($"Max: {max}");
         }
     }
 }
